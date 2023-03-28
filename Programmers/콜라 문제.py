77ff96ce026b0b0d61d5aeaaed78b1e01e9f2e28
yacho <cyh1219@naver.com>
2023-03-29 00:06:28 +0900

    Revert "feat(yacho)[add]: 콜라 문제 업로드. 재귀 불가 반복문 사용 (school.programmers.co.kr/learn/courses/30/lessons/132267)"
    This reverts commit 94c822ac066e2e01df68e9d1c6713e7b60b842f3.

--- Programmers/콜라 문제.py
+++
@@ -1,8 +0,0 @@
-def solution(a, b, n):
-    answer = 0
-    empty_bottles = n  
-    while empty_bottles >= a:  # a개 이상의 빈 병이 있을 때까지 반복
-        exchanged_bottles = empty_bottles // a  # a개의 빈 병으로 교환 가능한 콜라 병의 개수
-        answer += exchanged_bottles * b  # 콜라 병의 수 업데이트
-        empty_bottles = empty_bottles % a + exchanged_bottles * b  # 새로운 빈 병의 개수 업데이트
-    return answer
